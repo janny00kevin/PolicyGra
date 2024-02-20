99a0afb6b100f989dc9f035be8bbf8541e5be2b9
kevin <janny00kevin@gmail.com>
2024-02-20 19:06:41 +0800

    from laptop merging

--- test/test.py
+++ test/test.py
@@ -1,2 +1,20 @@
-import numpy
-print("hello warld") 
+import numpy as np
+# print("hello warld") 
+
+import torch
+from torch.distributions.categorical import Categorical
+
+logits = torch.tensor([ 0.25, 0.75 ])
+
+m = Categorical(probs = logits)
+print(m.log_prob())
+# for i in range(10000):
+    # assert m.sample().item() != 0, i
+
+    # if m.sample().item() == 0:
+    #     print(i)
+
+    # print(m.sample().item())
+# print(logits[1].type())
+
+# print(torch.tensor([ 0.1, 0.9, 0.3 ]).dim())
