26ca75d445e0c6b6ddf34dfc7b85b20fc4c73571
kevin <janny00kevin@gmail.com>
2024-02-21 12:41:25 +0800

    modify 1_simple_pg.py to make it works

--- test/test.py
+++ test/test.py
@@ -4,10 +4,16 @@ import numpy as np
 import torch
 from torch.distributions.categorical import Categorical
 
-logits = torch.tensor([ 0.25, 0.75 ])
+logits = torch.tensor([ 0.25, 0.2, 0.55 ])
 
 m = Categorical(probs = logits)
-print(m.log_prob())
+sample = m.sample()
+print(torch.exp(m.log_prob(sample)).item())
+
+a = torch.tensor([1,2,3.1])
+b = torch.tensor([4,5,6])
+# print((a*b).mean())
+print(torch.tensor([[1,2,3.01],[4,5,6]]).mean())
 # for i in range(10000):
     # assert m.sample().item() != 0, i
 
--- test/test2.py
+++ test/test2.py
@@ -1,4 +1,10 @@
 import numpy
+import torch
 
 print("hello world!")
 # #
+
+a = torch.tensor([1])
+print(a.item())
+
+b = [1, 2, 3]
