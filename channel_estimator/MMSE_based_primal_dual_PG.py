b676f31fbc058e2f50ccfbd156a395e28b1fb02c
kevin <janny00kevin@gmail.com>
2024-03-20 21:36:07 +0800

    add args, ep:10, hs:0.7, itr:1e4

--- channel_estimator/MMSE_based_primal_dual_PG.py
+++ channel_estimator/MMSE_based_primal_dual_PG.py
@@ -13,27 +13,27 @@ def mlp(sizes, activation=nn.Tanh, output_activation=nn.Softplus):
         layers += [nn.Linear(sizes[j], sizes[j+1]), act()]
     return nn.Sequential(*layers)
 
-def train(size, hidden_sizes=[64, 32], lr=1e-3, iteration=1000, batch_size=10, 
-          channel_information = [5, 0.2, 0, 0.01], epoch = 100):
+def train(size, hidden_sizes=[64, 32], lr=1e-3, num_iterations=1000, itr_batch_size=10, 
+          channel_information = [5, 0.2, 0, 0.01], num_epochs = 100, trajectory_size = 10):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    # device = torch.device("cpu")
+    device = torch.device("cpu")
     start_time = time.time()
     n_T, n_R, T = size
-    # generate the multilayer peceptron
 
     H_mean, H_sigma, W_mean, W_sigma = channel_information
     
     X = np.tile(np.eye(n_T), int(T/n_T))
-    print(X)
+    # print(X)
     
     epoch_loss = []
-    for i in range(epoch):
+    for i in range(num_epochs):
+        # generate the multilayer peceptron
         logits_net = mlp(sizes=[2*n_R*T]+hidden_sizes+[2*2*n_R*n_T]).to(device)
         optimizer = Adam(logits_net.parameters(), lr=lr)
         lamb, t = [1, 0]
         iter_loss = []
         iterations = []
-        for j in range(iteration):
+        for j in range(num_iterations):
             # generate the data
             H = np.random.normal(H_mean, H_sigma, [n_R,2*n_T]).view(np.complex128)
             W = np.random.normal(W_mean, W_sigma, [n_R,T*2]).view(np.complex128)
@@ -55,42 +55,45 @@ def train(size, hidden_sizes=[64, 32], lr=1e-3, iteration=1000, batch_size=10,
             for i in range(int(len(logits)/2)):
                 PG[i] = lamb*norm*Normal(logits[2*i],logits[2*i+1]).log_prob(h_hat[i])
             loss = PG.mean()
-            if (j)%batch_size == 0:
-                # print(j)
+            if (j)%itr_batch_size == 0:
                 iter_loss.append(norm)
                 iterations.append(j)
             loss.backward()
             optimizer.step()
             t -= lr*(1-lamb)
         epoch_loss.append(iter_loss)
-    
 
     end_time = time.time()
     print("time:", round((end_time-start_time),2), " sec")
     # plot the loss
     epoch_loss = np.array(epoch_loss).mean(-2)
     plt.plot(iterations, epoch_loss)
-    plt.title('epoch:%s, $[n_T,n_R,T]$:[%s,%s,%s], lr:%s' %(epoch,n_T,n_R,T,lr) )
+    plt.title('epoch:%s, $[n_T,n_R,T]$:[%s,%s,%s], lr:%s, sigma:%s' %(num_epochs,n_T,n_R,T,lr,H_sigma) )
     plt.xlabel('iterations')
     plt.ylabel('loss')
     plt.show()
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hs', type=float, default=0.4)
+    args = parser.parse_args()
+
     print(torch.cuda.is_available())
-    iteration = 20000
-    epoch = 1
-    batch_size = 50
-    n_T = 1
-    n_R = 1
-    T = n_T*1
-    size = [n_T, n_R, T]
+    num_iterations = 10000
+    num_epochs = 10
+    itr_batch_size = 50
+    trajectory_size = 5
+
     hidden_sizes=[64,32]
-    lr = 1e-3
-    H_mean = 5
-    H_sigma = 0.2
-    W_mean = 0
-    W_sigma = 0.1
-    channel_information = [H_mean, H_sigma, W_mean, W_sigma]
-    train(size, hidden_sizes, lr, iteration, batch_size, channel_information, epoch)
+    lr = 1e-4
+    
+    n_T, n_R= 1, 1
+    T = n_T*1
+    H_mean, H_sigma = 0, 0.6
+    W_mean, W_sigma = 0, 0.1
+
+    channel_information = [H_mean, args.hs, W_mean, W_sigma]
+    train([n_T, n_R, T], hidden_sizes, lr , num_iterations, itr_batch_size, channel_information, num_epochs, trajectory_size)
     
     
